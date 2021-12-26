5553a2863738fe84916d56a11a8ef33f04140a25
wangxincheng <15779742332@163.com>
2021-12-26 22:56:28 +0800

    提交

---
+++ app1/src/main.js
@@ -0,0 +1,43 @@
+// /src/main.js
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import singleSpaVue from 'single-spa-vue'
+ 
+Vue.config.productionTip = false
+ 
+const appOptions = {
+  el: '#microApp',
+  router,
+  render: h => h(App)
+}
+ 
+// 支持应用独立运行、部署，不依赖于基座应用。通俗来讲，就是在原有的项目基础上扩展，不侵入
+if (!window.singleSpaNavigate) {
+  delete appOptions.el
+  new Vue(appOptions).$mount('#app')
+}
+ 
+// 基于基座应用，导出生命周期函数
+const vueLifecycle = singleSpaVue({
+  Vue,
+  appOptions
+})
+ 
+export function bootstrap (props) {
+  console.log('app1 bootstrap')
+  console.log(props)
+  return vueLifecycle.bootstrap(() => {})
+}
+ 
+export function mount (props) {
+  console.log('app1 mount')
+  console.log(props)
+  return vueLifecycle.mount(() => {})
+}
+ 
+export function unmount (props) {
+  console.log('app1 unmount')
+  console.log(props)
+  return vueLifecycle.unmount(() => {})
+}
